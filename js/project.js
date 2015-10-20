aecf53fe7ec91b28157ae5c5e80084286279a37e
James Harris <computersherpa@gmail.com>
2015-10-19 23:39:25 -0500

    Readable time remaining

--- js/project.js
+++ js/project.js
@@ -1,13 +1,42 @@
 var minuteInMs = 1000 * 60;
 
+function readableTimespan(milliseconds) {
+   var secondSize = 1000;
+   var minuteSize = secondSize * 60;
+   var hourSize = minuteSize * 60;
+   var daySize = hourSize * 24;
+
+   var msRemaining = milliseconds;
+   var days = Math.floor(msRemaining / daySize);
+   msRemaining -= days * daySize;
+   var hours = Math.floor(msRemaining / hourSize);
+   msRemaining -= hours * hourSize;
+   var minutes = Math.floor(msRemaining / minuteSize);
+   msRemaining -= minutes * minuteSize;
+   var seconds = Math.floor(msRemaining / secondSize);
+
+   return {
+      seconds: seconds,
+      minutes: minutes,
+      hours: hours,
+      days: days,
+   };
+}
+
 function timerFactory(timespan) {
    var start = new Date();
    var distance = 25 * minuteInMs;
    var destination = new Date(start.valueOf() + distance);
-   console.log(destination);
+   return {
+      destination: destination,
+      read: function() {
+         var timeRemaining = this.destination - Date.now();
+         return readableTimespan(timeRemaining);
+      }
+   };
 }
 
-
 $("#timer1 .start-btn").click(function(){
-   timerFactory();
+   var timer = timerFactory();
+   console.log(timer.read());
 });
